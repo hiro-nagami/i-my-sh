6ba8a01f29169550be5e70c1e00e399af2858b40
h_nagami <hiro.nagami@gmail.com>
2017-02-07 12:43:21 +0900

    Rename zshrc and add script path

---
+++ my-zsh.sh
@@ -0,0 +1,9 @@
+# setup zsh command history
+export HISTFILE=${HOME}/.zsh_history
+export HISTSIZE=1000
+export SAVEHIST=100000
+
+CURRENT_DIR="$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)"
+
+# common alias
+source $CURRENT_DIR/alias.sh

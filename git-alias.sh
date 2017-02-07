7f8d67461ef0387d14f77581bab57e98bf4c16dc
h_nagami <hiro.nagami@gmail.com>
2017-02-07 16:23:33 +0900

    Add git-alias

---
+++ git-alias.sh
@@ -0,0 +1,8 @@
+#Alias (git)
+alias gst='git status'
+alias giff='git diff'
+alias gl='git log '
+alias gl1='gl --oneline'
+alias gb='git branch'
+alias gba='git branch -a'
+alias gv='git remote -v'
--- my-zsh.sh
+++ my-zsh.sh
@@ -7,3 +7,4 @@ CURRENT_DIR="$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)"
 
 # common alias
 source $CURRENT_DIR/alias.sh
+source $CURRENT_DIR/git-alias.sh

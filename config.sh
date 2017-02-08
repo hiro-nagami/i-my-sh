e84ed5374986226ac3603c09530764c33a04a56b
h_nagami <hiro.nagami@gmail.com>
2017-02-08 11:06:16 +0900

    Add setup and config sh

---
+++ config.sh
@@ -0,0 +1,15 @@
+# setup path
+export CURRENT_DIR="$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)"
+
+# GO
+export GOPATH=$HOME/.go
+export GOROOT=/usr/local/opt/go/libexec
+export PATH=$PATH:$GOPATH/bin:$GOROOT/bin
+
+# git
+export GIT_REPOS=$HOME/.git
+
+# setup zsh command history
+export HISTFILE=${HOME}/.zsh_history
+export HISTSIZE=1000
+export SAVEHIST=100000
---
+++ setup.sh
@@ -0,0 +1,40 @@
+{
+	source "$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/config.sh"
+
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+	brew install go
+	brew install npm
+	brew install peco
+	brew tap Homebrew/bundle
+
+	git clone https://github.com/git/git.git $GIT_REPOS
+
+	mkdir $GOPATH
+
+	go get github.com/motemen/ghq
+
+	rm -rf $GIT_REPOS
+	ghq get https://github.com/git/git.git
+	ghq get https://github.com/hiro-nagami/mysh.git
+
+	ln -s $HOME/.ghq/github.com/git/git $GIT_REPOS
+	ln -s $HOME/.ghq/github.com/hiro-nagami/mysh $MYSH_DIR/.mysh
+
+	ghq get https://github.com/zsh-users/zsh-autosuggestions.git
+	ghq get https://github.com/sstephenson/rbenv.git
+	ghq get https://github.com/sstephenson/ruby-build.git
+
+	ln -s $HOME/.ghq/github.com/sstephenson/rbenv $HOME/.rbenv
+	ln -s $HOME/.ghq/github.com/sstephenson/ruby-build $HOME/.rbenv/plugins/ruby-build
+
+	ssh-keygen -t rsa
+
+    if [ -z "$SOURCED_MYSH" ] && [ "${SOURCED_MYSH:-A}" = "${SOURCED_MYSH-A}" ]
+        echo 'source $MYSH_DIR/my-zsh.sh' >> $HOME/.zshrc
+        echo 'SOURCED_MYSH=true' >> $HOME/.zshrc
+    fi
+    
+	zsh
+	source $HOME/.zshrc
+}

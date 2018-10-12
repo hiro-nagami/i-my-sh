1a4890d1cdf768a0cc921d3849a98737ad88e697
HIROMASA NAGAMINE <hiro.nagami@gmail.com>
2018-10-12 15:41:49 +0900

    Add /usr/local/bin to PATH

--- my-zsh.sh
+++ my-zsh.sh
@@ -38,7 +38,7 @@ COMPLETION_WAITING_DOTS=true
 # zle -N zle-line-init
 
 ## rbenv
-export PATH="$HOME/.rbenv/bin:$PATH"
+export PATH=/usr/local/bin:$HOME/.rbenv/bin:$PATH
 eval "$(rbenv init -)"
 
 ## openssl
